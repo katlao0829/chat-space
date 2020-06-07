a2c12a02fbc7b7c5c59f68f66186e335efc0a75c
katlao0829 <nskj.m1958@gmail.com>
2020-06-07 15:58:58 +0900

    Set controller for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,14 @@
 class MessagesController < ApplicationController
+ 
   def index
   end
+  def create
+    @messages = Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id, group_id: params[:group_id])
+
+
 end
